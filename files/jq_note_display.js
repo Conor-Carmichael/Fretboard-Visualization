b493d56f73e40310781d3b3d2ab055de6977c842
Conor Carmichael <ccarmichael@umass.edu>
2017-10-19 20:31:26 -0400

    Initial commit of project.

---
+++ files/jq_note_display.js
@@ -0,0 +1,149 @@
+var color_counter=0;
+var bebop;
+var blues;
+
+$(document).ready(function(){
+    $( function() {
+        $( "#chordsInScale, #currentProgression" ).sortable({
+            connectWith: ".connectedSortable"
+        }).disableSelection();
+    } );
+    
+
+    $('#modeAndNoteSubmit').click(function(){
+        clearBoard();
+
+        $('#currentProgression').append("<li>Drag&Drop to Build \nProgressions Here:</li>");
+        var root = $('#notes').val();
+        var scale_mode = $('#modes').val();
+        var arr = scale_create(root,scale_mode);
+        addHeading(arr, root, scale_mode);
+        var objTemp = chordsList(arr,scale_mode);
+        
+        appendKeys(objTemp);
+        removeSharps(arr);
+        showNote(arr);
+        //create chord list, append to ul on left of screen, make draggable into  list for timings and progressions
+        
+    });
+
+    $(document).on('mouseenter','.chordFromScale',function(){
+        var notes = $(this).attr('id').split(',');
+        removeSharps(notes);
+        highlight(notes);
+    });
+    $(document).on('mouseleave','.chordFromScale',function(){
+        var notes = $(this).attr('id').split(',');
+        removeSharps(notes);
+        dehighlight(notes);
+    });
+    
+    $(document).on('mouseenter','.chordFromScale',function(){
+        var notes = $(this).attr('id').split(',');
+        removeSharps(notes);
+        highlight(notes);
+    });
+    
+    
+    
+    
+    
+    $("#clearBoard").click(function(){
+        clearBoard();
+    });    
+});// END DOC READY
+
+ 
+
+
+function addHeading(scale, root, mode){
+    $('h2').text(root+" "+mode+":");
+    $('#scalePara').text(scale.toString());
+}
+function appendKeys(chordObj){
+    for(var i=0;i<Object.keys(chordObj).length; i++){
+        $('#chordsInScale').append("<li class='chordFromScale' id='"+chordObj[index_list[String(i)]]+"'>"+index_list[String(i)]+": "+chordObj[index_list[String(i)]]+"</li>");
+//        $('#currentProgression').append("<li>"+index_list[String(i)]+": "+chordObj[index_list[String(i)]]+"</li>");
+
+    }
+}   
+
+
+
+
+function clearBoard(){
+    color_counter=0;
+    $('#lowestring li').animate({opacity:0});
+    $('#astring li').animate({opacity:0});
+    $('#dstring li').animate({opacity:0});
+    $('#gstring li').animate({opacity:0});
+    $('#bstring li').animate({opacity:0});
+    $('#highestring li').animate({opacity:0});
+    $('#chordsInScale').empty();
+    $('#currentProgression').empty();
+    $('h2').text("");
+    $('#scalePara').text("");
+    $('.E').css({'background':'#211f1d'});
+    $('.A').css({'background':'#211f1d'});
+    $('.D').css({'background':'#211f1d'});
+    $('.G').css({'background':'#211f1d'});
+    $('.B').css({'background':'#211f1d'});
+}
+
+
+function dehighlight(notes){
+    for(var i=0;i<notes.length;i++){
+        $('.'+notes[i]+'').css({'background':'rgba(220, 183, 36,1)'});
+    }
+}
+
+function highlight(notes){
+    for(var i=0;i<notes.length;i++){
+        $('.'+notes[i]+'').css({'background':'#9D002C'});
+        
+    }
+}
+
+
+function showNote(notes){
+    for(var i=0;i<notes.length;i++){
+            $('.'+notes[i]+'').animate({opacity:1});
+            $('.'+notes[i]+'').css({'background':'rgba(220, 183, 36,1)'});
+    }
+}
+
+function removeSharps(arr_){
+    for(var c=0;c<arr_.length;c++){
+        arr_[c]=arr_[c].replace(/#/g, "sharp");
+    }
+}
+
+function addSharps(arr_){
+    for(var c=0;c<arr_.length;c++){
+        arr_[c]=arr_[c].replace(/sharp/g, "#");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ files/noteObtain.js
@@ -0,0 +1,184 @@
+//ported code from og python script
+/* CODE TO CREATE SCALES, PATTERNS FOR DIFFERENT MODES *************************************************************************************************************************************************************/
+var notes_arr = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+var notes_length = notes_arr.length;
+var modes = ['MAJOR', 'DORIAN', 'PHRYGIAN', 'LYDIAN', 'MIXOLYDIAN', 'MINOR', 'LOCRIAN'];
+var whole_step  = 2;
+var half_step   = 1;
+var scale_pattern = [whole_step, whole_step, half_step, whole_step, whole_step, whole_step, half_step];
+var pentMaj=[]
+
+var finalList={};
+var index_list={};
+
+//gets pattern for a mode, bascially just rotates the array scale_pattern
+function modePattern(mode){
+	var return_list = new Array(7);
+	var mode_index = modes.indexOf(mode);
+    var i;
+    for(i=1; i<scale_pattern.length; i++){
+        return_list[i] = (scale_pattern[mode_index%scale_pattern.length]);
+		mode_index++;
+    }
+//    alert(return_list);
+	return return_list;
+}
+
+//creates the scale given a root note and mode
+//gets pattern from above, goes through notes_arr[] starting at the index of the root, going to the next step(whole/half) in the notes based off pattern
+function scale_create(root, mode){
+    if(mode === 'MAJOR-PENT'){
+        var realMode='MAJOR';
+        var pattern = modePattern(realMode);
+        var note_index = notes_arr.indexOf(root);
+        var notes_return=new Array(scale_pattern.length);
+        notes_return[0] = root;
+        var i;
+        for(i=1; i<pattern.length ; i++){
+            note_index += pattern[i];
+            notes_return[i] = notes_arr[note_index%notes_arr.length];
+        }
+        var majorArr=[notes_return[0],notes_return[1],notes_return[2],notes_return[4],notes_return[5]]
+        return majorArr;
+    }
+    else if(mode==='MINOR-PENT'){
+         var realMode='MINOR';
+        var pattern = modePattern(realMode);
+        var note_index = notes_arr.indexOf(root);
+        var notes_return=new Array(scale_pattern.length);
+        notes_return[0] = root;
+        var i;
+        for(i=1; i<pattern.length ; i++){
+            note_index += pattern[i];
+            notes_return[i] = notes_arr[note_index%notes_arr.length];
+        }
+        var minorArr=[notes_return[0],notes_return[2],notes_return[3],notes_return[4],notes_return[6]]
+        return minorArr;
+    }
+    else if(mode==='MAJ-MIN-PENT'){
+        var maj = scale_create(root, 'MAJOR-PENT');
+        var min = scale_create(root, 'MINOR-PENT');
+        var combine = maj.concat(min);
+        alert(combine);
+        return combine;
+        
+    }
+    else{
+        var pattern = modePattern(mode);
+        var note_index = notes_arr.indexOf(root);
+        var notes_return=new Array(scale_pattern.length);
+
+        notes_return[0] = root;
+        var i;
+        for(i=1; i<pattern.length ; i++){
+            note_index += pattern[i];
+            notes_return[i] = notes_arr[note_index%notes_arr.length];
+        }
+        return notes_return;
+    }
+}
+
+
+/* START OF CHORD FUNCTIONS  *******************************************************************************************************************************************************************************************/
+var chord_functions={
+    //chords
+    'maj_chord':function(scale){
+        return [scale[0],scale[2],scale[4]];
+    },
+    'min_chord':function(scale){
+        return [scale[0],notes_arr[notes_arr.indexOf(scale[2])-1],scale[4]];
+    },
+    'sus2_chord':function (scale){
+        return [scale[0],scale[1],scale[4]];
+    },
+    sus4_chord:function(scale){
+        return [scale[0],scale[3],scale[4]];
+    },
+    augmented_chord: function(scale){	
+        return [scale[0],scale[2],notes_arr[(notes_arr.indexOf(scale[4])+1)%notes_length]];
+    },
+    diminished_chord:function(scale){
+        return [scale[0],notes_arr[notes_arr.indexOf(scale[2])-1],notes_arr[notes_arr.indexOf(scale[4])-1]];
+    },
+    //seventh chords
+    maj_seventh_chord:function(scale){
+        return [scale[0],scale[2],scale[4], scale[6]];
+    },
+    min_seventh_chord:function(scale){
+        return [scale[0],notes_arr[notes_arr.indexOf(scale[2])-1],scale[4], notes_arr[notes_arr.indexOf(scale[6])-1]];
+    },
+    seventh_chord:function(scale){ 	
+        return [scale[0],scale[2],scale[4], notes_arr[notes_arr.indexOf(scale[6])-1]];
+    },
+    min_seventh_flat_five_chord:function(scale){	
+        return [scale[0],notes_arr[notes_arr.indexOf(scale[2])-1],notes_arr[notes_arr.indexOf(scale[4])-1], notes_arr[notes_arr.indexOf(scale[6])-1]];
+    },
+    dim_seventh_chord:function(scale){
+        return [scale[0],notes_arr[notes_arr.indexOf(scale[2])-1],notes_arr[notes_arr.indexOf(scale[4])-1], notes_arr[notes_arr.indexOf(scale[6])-2]];
+    },
+    //sixth chords
+    sixth_chord:function(scale){
+        return [scale[0],scale[2],scale[4], scale[5]];
+    },
+    min_sixth_chord:function(scale){
+        return [scale[0],notes_arr[notes_arr.indexOf(scale[2])-1],scale[4], scale[6]];
+    },
+    //other
+    add_nine_chord:function(scale){
+        return [scale[0],scale[2],scale[4], scale[9%scale.length]];
+    }
+}
+var chord_functions_array =['maj_chord', 'min_chord', 'sus2_chord', 'sus4_chord', 'augmented_chord', 'diminished_chord', 'maj_seventh_chord',
+				'min_seventh_chord', 'seventh_chord', 'min_seventh_flat_five_chord', 'dim_seventh_chord', 'sixth_chord', 'min_sixth_chord', 'add_nine_chord'];
+
+
+
+function arrComp(scale, chord){
+    for(var i=0;i<chord.length;i++){
+        if(scale.indexOf(chord[i])===-1)
+            return false;
+    }
+    return true;
+}
+//----------------------------IF YOU ADD CHORDS ADD TO FUNCTIUONS AND CHORD LIST AT SAME PLACE__________________________________________________
+
+
+function chordsList(initial_scale, mode){
+    finalList={};
+    index_list={};
+    var added=0;
+    for(var j=0;j<initial_scale.length;j++){    
+        var curr_scale = scale_create(initial_scale[j],'MAJOR');
+        for(var i=0;i<chord_functions_array.length;i++){
+            var tempChord= chord_functions[chord_functions_array[i]](curr_scale);
+            
+            if(arrComp(initial_scale,tempChord)){
+                var formatted_name = curr_scale[0]+" "+chord_name_toString(chord_functions_array[i]);
+                finalList[formatted_name]=tempChord;
+                index_list[added++]=formatted_name;
+//                alert(finalList[0]);
+            }
+        }
+    }
+    alert("test"+JSON.stringify(index_list));
+    alert(added+'\n'+finalList.toSource());
+
+    return finalList;
+}
+
+
+function chord_name_toString(chord){
+    chord = chord.replace(/_/g, " ");
+    chord = chord.replace(/chord/g, "");
+    chord = chord.replace(/min/g, "Minor");
+    chord = chord.replace(/maj/g, "Major");    
+    return chord;
+}
+
+
+
+
+
+
+
+
